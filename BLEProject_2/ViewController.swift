b6d1472d3d4579d8dc6e238ad86e5c0b9ed3459f
Mohammed Ashfaq <ashfaq@mohammeds-macbook-pro.local>
2018-10-03 14:58:15 +0300

    Initial Commit

---
+++ BLEProject_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BLEProject_2
+//
+//  Created by Mohammed Ashfaq on 23/01/1440 AH.
+//  Copyright © 1440 Mohammed Ashfaq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
